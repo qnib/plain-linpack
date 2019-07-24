714bd1d060d73e6f081b5ef38ad806c038533f7e
Christian Kniep <christian@qnib.org>
2019-07-24 17:27:32 +0200

    updated AMD file

---
+++ env/r2700x.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export HPL_N=29464
+export HPL_NB=232
+export HPL_P=2
+export HPL_Q=4
+export OMP_NUM_THREADS=1
+export MPI_NUM_RANKS=8
--- hpl/bench.sh
+++ hpl/bench.sh
@@ -2,8 +2,20 @@
 
 : "${OMP_PROC_BIND:=TRUE}"
 : "${OMP_PLACES:=cores}"
-: "${OMP_NUM_THREADS:=4}"
-: "${MPI_NUM_RANKS:=4}"
+
+if [[ "X${HPL_ENV_FILE}" == "X" ]];then
+    echo '!! Please provide environment file to source  via environment $HPL_ENV_FILE (eg. r2700x.sh)'
+    exit 1
+
+fi
+echo ">> Source $HPL_ENV_FILE"
+source $HPL_ENV_FILE
+echo ">> N=${HPL_N} / NB=${HPL_NB} / P=${HPL_P} / Q=${HPL_Q}"
+echo ">> OMP_NUM_THREADS=${OMP_NUM_THREADS} / MPI_NUM_RANKS=${MPI_NUM_RANKS}"
+
+echo ">> Update HPL.dat"
+confd -onetime -backend env
 
 echo ">> mpirun --allow-run-as-root -np ${MPI_NUM_RANKS} --map-by l3cache --mca btl self,vader xhpl"
 mpirun --allow-run-as-root -np ${MPI_NUM_RANKS} --map-by l3cache --mca btl self,vader xhpl
+exit 0
